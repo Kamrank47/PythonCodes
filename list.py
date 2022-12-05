1a703c75c30b3faed42d44a16fb3dc693e9a199a
Kamran <kamranali.k47@gmail.com>
2022-12-05 22:45:22 +0500

    list completed

---
+++ list.py
@@ -0,0 +1,16 @@
+# declare array
+
+firstList = ["john",31,"03004445678",]
+
+#add value in list
+
+firstList.append("Naughty")
+
+# remove value from end of the list 
+
+firstList.pop()
+
+# print elements of array
+
+for listvalue in firstList:
+    print(f"value at {firstList.index(listvalue)} is {listvalue}")
